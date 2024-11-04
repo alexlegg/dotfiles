c77096b1a71abb72d55e86431e34f762c7e1d8ec
Alex Legg <alexl@apple.com>
2024-11-04 12:56:26 +1100

    Add morlana

--- flake.nix
+++ flake.nix
@@ -4,11 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
+    # nix-darwin contains nix modules for darwin. Everything is based on this.
     darwin = {
       url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Morlana implements utilities nix-darwin in Rust. It has nicer output.
+    morlana = {
+      url = "github:ryanccn/morlana";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Snowfall is the library that opinionates the layout of this repo.
     snowfall-lib = {
         url = "github:snowfallorg/lib";
         inputs.nixpkgs.follows = "nixpkgs";
@@ -24,6 +32,10 @@
       channels-config = {
         allowUnfree = true;
       };
+
+      overlays = with inputs; [
+      	morlana.overlays.default
+      ];
 
       snowfall = {
         namespace = "dotfiles";
