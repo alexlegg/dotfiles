c6568d7e7d27a9048289ef3b623a669a01caca6b
Alex Legg <alexl@apple.com>
2025-04-11 08:18:50 +1000

    Add nito to wezterm ssh config

--- modules/home/packages/wezterm/default.nix
+++ modules/home/packages/wezterm/default.nix
@@ -96,6 +96,12 @@ in {
               username = "alexl",
               remote_wezterm_path = '/Applications/Nix\\ User\\ Apps/WezTerm.app/Contents/MacOS/wezterm',
           },
+          {
+              name = "nito",
+              remote_address = "nito",
+              username = "alexl",
+              remote_wezterm_path = '/Applications/Nix\\ User\\ Apps/WezTerm.app/Contents/MacOS/wezterm',
+          },
       }
 
       -- and finally, return the configuration to wezterm
