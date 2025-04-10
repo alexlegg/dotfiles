da8f451daf9444a099266347a5d464fc1379fa83
Alex Legg <alexl@apple.com>
2025-04-11 08:16:31 +1000

    Update wezterm hash

--- packages/wezterm-binary/default.nix
+++ packages/wezterm-binary/default.nix
@@ -12,7 +12,7 @@ stdenvNoCC.mkDerivation rec {
     #url = "https://github.com/wez/wezterm/releases/download/${version}/WezTerm-macos-${version}.zip";
     # Get the nightly instead
     url = "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-macos-nightly.zip";
-    hash = "sha256-4r1NJzCP5J84heaKlIcbSZAlE5ewXNw2ItcT32LGK3U=";
+    hash = "sha256-mYX88EehRet98Lk1NoVvjSfustAG7Q2U90A0UGHRDMI=";
   };
 
   installPhase = ''
